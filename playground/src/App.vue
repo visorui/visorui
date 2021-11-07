803069c33d953b5931a4bc233a386241a1c11330
Marvin Rudolph <rudolph.marvin@web.de>
2021-11-07 15:41:37 +0100

    chore: modal playground tests

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Modal, ModalOverlay, ModalTitle } from '@visorui/core'
+import {
+  Modal,
+  ModalOverlay,
+  ModalTitle,
+  ModalDescription
+} from '@visorui/core'
 
 const isVisible = ref(false)
 const openModal = () => (isVisible.value = true)
@@ -11,15 +16,45 @@ const openModal = () => (isVisible.value = true)
     class="fixed inset-0 z-20 flex items-center justify-center"
     v-model="isVisible"
   >
-    <ModalOverlay class="fixed inset-0 bg-black bg-opacity-50" />
-    <div class="bg-white relative z-10 rounded-xl p-20 max-w-md w-full">
-      <ModalTitle>Hello</ModalTitle>
-      <form>
-        <input type="text" class="border-2" />
-      </form>
-    </div>
+    <transition name="fade" appear>
+      <ModalOverlay
+        class="fixed inset-0 bg-black bg-opacity-50"
+        v-if="isVisible"
+      />
+    </transition>
+    <transition name="slide" appear>
+      <div class="bg-white relative z-10 rounded-xl p-20 max-w-md w-full">
+        <ModalTitle>Hello!</ModalTitle>
+        <ModalDescription>Modal description</ModalDescription>
+        <form>
+          <input type="text" class="border-2" />
+        </form>
+      </div>
+    </transition>
   </Modal>
   <p>
     <button @click="openModal">Open Modal</button>
   </p>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.3s ease;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateY(-10%);
+}
+</style>
