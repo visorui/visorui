d060d728f1397e3e4ffeeb16fdf12e750ebb32be
Marvin Rudolph <rudolph.marvin@web.de>
2021-11-11 19:59:58 +0100

    chore: adjust eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     'import/first': 'error',
     'import/no-unresolved': 'off',
     'vue/require-default-prop': 'off',
-    'vue/multi-word-component-names': 'off'
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-empty-function': 'off'
   }
 }
