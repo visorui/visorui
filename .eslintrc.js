14a282e3e5f12f6b07ee01cca5aacd522d9ce43f
Marvin Rudolph <rudolph.marvin@web.de>
2021-11-22 16:19:42 +0100

    chore: force error of no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,8 @@ module.exports = {
     'import/no-unresolved': 'off',
     'vue/require-default-prop': 'off',
     'vue/multi-word-component-names': 'off',
-    '@typescript-eslint/no-empty-function': 'off'
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'error'
   },
   overrides: [
     {
